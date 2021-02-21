b1b34ff30ef106ca0dc89ae3818a3be938c7b2f6
ShehabEMohsen <65345727+ShehabEMohsen@users.noreply.github.com>
2021-02-21 21:56:54 +0200

    Test Final Project

---
+++ Travel booking management system/src/travel/booking/management/system/TravelBookingManagementSystem.java
@@ -0,0 +1,12 @@
+
+package travel.booking.management.system;
+
+
+public class TravelBookingManagementSystem {
+
+    
+    public static void main(String[] args) {
+       GUI f= new GUI();
+    }
+    
+}
